83e0539dc150beccda319e9a30bb7f267b32409e
Reza Achmad Naufal <reza.naufal.naufal@gmail.com>
2022-01-23 07:28:26 +0700

    fix ios

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@
  * @format
  */
 
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import {
   AppRegistry,
   SafeAreaView,
@@ -23,17 +23,36 @@ import {
 
 import {
   Colors,
-  DebugInstructions,
   Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
 import codePush from 'react-native-code-push';
 
+const isProduction = false;
+
+const deploymentKey_stagging =
+  Platform.OS === 'ios'
+    ? 'HSS6U3d_kU1_LnJzWZ7y0zbPYeNIrvDuZun_S'
+    : '6TJW0uoxkEEgbsZNazZfYbdtQR_i31MggXiXd';
+
+const deploymentKey_production =
+  Platform.OS === 'ios'
+    ? 'm8SRoZPxGp4xYAgcAXR7V7euSUfwtFvhW13ZE'
+    : 'XVaJYGRZP_rFzdiT2XdeuBLBgtqIKbwS4NogX';
+
+let codePushOptions = {
+  deploymentKey: isProduction
+   ? deploymentKey_production
+   : deploymentKey_stagging,
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+  installMode: codePush.InstallMode.ON_NEXT_RESUME,
+};
+
 const Section: React.FC<{
   title: string;
 }> = ({children, title}) => {
+
+  
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -59,9 +78,59 @@ const Section: React.FC<{
   );
 };
 
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {  
 
+  const [status, setStatus] = useState("");
+
+  function codePushStatusDidChange(syncStatus: any) {
+    useContext
+    switch(syncStatus) {
+      case codePush.SyncStatus.CHECKING_FOR_UPDATE:
+        // AlertIOS.alert({ syncMessage: "Checking for update." });
+        setStatus("Checking for update.")
+        break;
+      case codePush.SyncStatus.DOWNLOADING_PACKAGE:
+        // console.log({ syncMessage: "Downloading package." });
+        setStatus("Downloading package.")
+        break;
+        case codePush.SyncStatus.AWAITING_USER_ACTION:
+          console.log({ syncMessage: "Awaiting user action." });
+          setStatus("Awaiting user action.")
+          break;
+      case codePush.SyncStatus.INSTALLING_UPDATE:
+        console.log({ syncMessage: "Installing update." });
+        setStatus("Installing update.")
+        break;
+      case codePush.SyncStatus.UP_TO_DATE:
+        console.log({ syncMessage: "App up to date.", progress: false });
+        setStatus("App up to date.")
+        break;
+      case codePush.SyncStatus.UPDATE_IGNORED:
+        console.log({ syncMessage: "Update cancelled by user.", progress: false });
+        setStatus("Update cancelled by user.")
+        break;
+      case codePush.SyncStatus.UPDATE_INSTALLED:
+        console.log({ syncMessage: "Update installed and will be applied on restart.", progress: false });
+        setStatus("Update installed and will be applied on restart.")
+        break;
+      case codePush.SyncStatus.UNKNOWN_ERROR:
+        console.log({ syncMessage: "An unknown error occurred.", progress: false });
+        setStatus("An unknown error occurred.")
+        break;
+    }
+  }
+
+  function codePushDownloadDidProgress(progress: any) {
+    console.log({ progress });
+  }
+
+    codePush.sync(
+      codePushOptions,
+      codePushStatusDidChange.bind(this),
+      codePushDownloadDidProgress.bind(this)
+    );
+  const isDarkMode = useColorScheme() === 'dark';
+  
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
@@ -77,47 +146,17 @@ const App = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>Index.tsx</Text> to change this
-            screen and then come back to see your edits.
+          <Section title="Codepush Status">
+            {status}
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
+          <Section title="Changes">
+            <Text style={styles.highlight}>Updated textppp</Text>
           </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 };
-
-const isProduction = false;
-
-const deploymentKey_stagging =
-  Platform.OS === 'ios'
-    ? 'HSS6U3d_kU1_LnJzWZ7y0zbPYeNIrvDuZun_S'
-    : '6TJW0uoxkEEgbsZNazZfYbdtQR_i31MggXiXd';
-
-const deploymentKey_production =
-  Platform.OS === 'ios'
-    ? 'm8SRoZPxGp4xYAgcAXR7V7euSUfwtFvhW13ZE'
-    : 'XVaJYGRZP_rFzdiT2XdeuBLBgtqIKbwS4NogX';
-
-let codePushOptions = {
-  deploymentKey: isProduction
-    ? deploymentKey_stagging
-    : deploymentKey_production,
-  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
-  installMode: codePush.InstallMode.ON_NEXT_RESUME,
-};
-
-const APP = codePush(codePushOptions)(App);
 
 const styles = StyleSheet.create({
   sectionContainer: {
@@ -138,6 +177,6 @@ const styles = StyleSheet.create({
   },
 });
 
-export default APP;
+export default App;
 
-AppRegistry.registerComponent('reactcodepush', () => APP);
+AppRegistry.registerComponent('reactcodepush', () => App);
--- ios/reactcodepush/AppDelegate.m
+++ ios/reactcodepush/AppDelegate.m
@@ -4,6 +4,11 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <CodePush/CodePush.h>
+#import <AppCenterReactNative.h>
+#import <AppCenterReactNativeAnalytics.h>
+#import <AppCenterReactNativeCrashes.h>
+
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
@@ -27,6 +32,10 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+[AppCenterReactNative register];
+[AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
+[AppCenterReactNativeCrashes registerWithAutomaticProcessing];
+
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
@@ -47,6 +56,7 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+
   return YES;
 }
 
@@ -55,7 +65,7 @@ static void InitializeFlipper(UIApplication *application) {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  return [CodePush bundleURL];
 #endif
 }
 
